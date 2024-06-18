0ac3449172be894a2388d10f912ddb64a23102d3
ProjetoIFPI <proje1oifpi@gmail.com>
2024-06-18 20:21:26 -0300

    Update app.js

--- Login/js/app.js
+++ Login/js/app.js
@@ -3,10 +3,17 @@ var btnInscrever = document.querySelector("#inscrever");
 
 var body = document.querySelector("body");
 
-btnEntrar.addEventListener("click", function(){
+btnEntrar.addEventListener("click", function() {
     body.className = "entrar-js";
 });
 
-btnInscrever.addEventListener("click", function(){
+btnInscrever.addEventListener("click", function() {
     body.className = "inscrever-js";
+});
+
+var loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.location.href = "home.html";
 });
